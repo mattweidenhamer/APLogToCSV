2f786425fba87e51f53ca4e179077574aead228f
Matthew Weidenhamer <mattweidenhamer@gmail.com>
2024-12-02 14:18:41 -0500

    Initial Commit

---
+++ parseAPLog.py
@@ -0,0 +1,18 @@
+
+
+def promptForFile():
+    # Prompts the user for the log file they would like to parse.
+    pass
+
+
+def cleanLog():
+    # Removes unneccesary information from the 
+    pass
+
+def parseLog():
+    # Takes an Archipelago log file and converts it into a CSV for analysis.
+    pass
+
+
+if __name__ == "__main__":
+    promptForFile()
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Convert Archipelago Log to CSV.
+
+A tool to convert the output files from Archipelago into 
+
+How to use:
+1. Find the `items.csv` file from your apworld and put it into the `items` folder.
+2. Put your world log into the `input` folder.
+3. Run the file. You'll find the result in the output folder.
