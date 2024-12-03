df7819c85e3f11b0975a280ce8bd0976654033f8
Matthew Weidenhamer <mattweidenhamer@gmail.com>
2024-12-03 13:29:08 -0500

    Added notice parsing

--- parse_ap_log.py
+++ parse_ap_log.py
@@ -21,6 +21,9 @@ def preprocess_line(line: str) -> str:
     # Ignore the line if it isn't timestamped (such as commands)
     if not line.startswith("["):
         return None
+    
+    # return the line if it has a valid timestamp.
+    return line
 
 def split_timestamp(line:str) -> Tuple[str, str]:
     # Splits the timestamp from the rest of the line.
@@ -35,16 +38,42 @@ def parse_notice(notice: str):
 
     # Notice CSV file format:
     # Timestamp: When the notice was sent
-    # Event: Join, leave, or view.
-    # Sender: Who sent the notice
-    # Team: The team that the notice is about
+    # Event: Join or leave.
+    # Player: Who sent the notice
+    # Player Team: The team that the sender is on
     # Example input line: [2024-11-29 05:34:12,250]: Notice (all): surger1 (Team #1) playing Stardew Valley has joined. Client(0.5.0), ['AP'].
 
     # If the notice is not a join or leave notice, ignore it.
     if "has joined" not in notice and "has left" not in notice:
         return None
     
+    # By default, ignore the notice if it for a viewer instead of a player.
+    if "'TextOnly'" in notice:
+        return None
     
+    # Split the timestamp from the rest of the line.
+    timestamp, notice = split_timestamp(notice)
+
+    # Remove the notice from the line.
+    notice = notice.split(":", 1)[1]
+
+    # Divide the playing half and the notice half, to avoid accidental misinterpretation.
+    player, game = notice.split(" playing ", 1)
+
+    player, team = player.split(" (Team ", 1)
+    player.strip()
+    team = team[:-1]
+    team.strip()
+
+    # The event is either join or leave.
+    event = "join" if "has joined" in game else "leave"
+    
+    return {
+        "timestamp": timestamp,
+        "event": event,
+        "player": player,
+        "player_team": team,
+    }
 
 def parse_item_line(item_line: str):
     # Parses an item send line into a set of CSV values.
